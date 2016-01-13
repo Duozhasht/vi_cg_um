81bc60938669fc7997fcc62c991fc0eab9028b14
Carlos Brito <cfabrito@gmail.com>
2016-01-13 17:40:52 +0000

    Atmosphere shaders and initial index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var canvas = document.body.appendChild(document.createElement('canvas'))
 // Creates an instance of canvas-orbit-camera,
 // which later will generate a view matrix and
 // handle interaction for you.
-var camera = require('canvas-orbit-camera')(canvas)
+// var camera = require('canvas-orbit-camera')(canvas)
 
 // A small convenience function for creating
 // a new WebGL context – the `render` function
@@ -26,25 +26,7 @@ var gl = require('gl-context')(canvas, render)
 window.addEventListener('resize'
   , fit(canvas)
   , false
-)
-
-// Load the bunny mesh data (a simplicial complex)
-// into a gl-geometry instance, calculating vertex
-// normals for you. A simplicial complex is simply
-// a list of vertices and faces – conventionally called
-// `positions` and `cells` respectively. If you're familiar
-// with three.js, this is essentially equivalent to an array
-// of `THREE.Vector3` and `THREE.Face3` instances, except specified
-// as arrays for simplicity and interoperability.
-var geometry = Geometry(gl)
-
-geometry.attr('aPosition', bunny.positions)
-geometry.attr('aNormal', normals.vertexNormals(
-    bunny.cells
-  , bunny.positions
-))
-
-geometry.faces(bunny.cells)
+)	
 
 // Create the base matrices to be used
 // when rendering the bunny. Alternatively, can
@@ -63,14 +45,17 @@ var width
 // step ahead of time. We can make some dramatic file size
 // savings by doing this in Node rather then at runtime in
 // the browser.
-var shader = glShader(gl,
-    glslify('./shaders/bunny.vert')
-  , glslify('./shaders/bunny.frag')
+var program = glShader(gl,
+    glslify('./shaders/example.vert')
+  , glslify('./shaders/example.frag')
 )
 
 // The logic/update loop, which updates all of the variables
 // before they're used in our render function. It's optional
 // for you to keep `update` and `render` as separate steps.
+
+var theta;
+
 function update() {
   // Updates the width/height we use to render the
   // final image.
@@ -78,55 +63,71 @@ function update() {
   height = gl.drawingBufferHeight
 
   // Updates our camera view matrix.
-  camera.view(view)
+  // camera.view(view)
 
   // Optionally, flush the state of the camera. Required
   // for user input to work correctly.
-  camera.tick()
+  // camera.tick()
 
   // Update our projection matrix. This is the bit that's
   // responsible for taking 3D coordinates and projecting
   // them into 2D screen space.
-  var aspectRatio = gl.drawingBufferWidth / gl.drawingBufferHeight
-  var fieldOfView = Math.PI / 4
-  var near = 0.01
-  var far  = 100
+  // var aspectRatio = gl.drawingBufferWidth / gl.drawingBufferHeight
+  // var fieldOfView = Math.PI / 4
+  // var near = 0.01
+  // var far  = 100
 
-  mat4.perspective(projection
-    , fieldOfView
-    , aspectRatio
-    , near
-    , far
-  )
+  // mat4.perspective(projection
+  //   , fieldOfView
+  //   , aspectRatio
+  //   , near
+  //   , far
+  // )
+  
+  theta += 0.0125;
 }
+
+var quad = Geometry(gl)
+    .attr('aPosition', [
+        -1, -1, -1,
+         1, -1, -1,
+         1,  1, -1,
+        -1, -1, -1,
+         1,  1, -1,
+        -1,  1, -1
+    ]);
 
 function render() {
   update()
 
   // Sets the viewport, i.e. tells WebGL to draw the
   // scene across the full canvas.
-  gl.viewport(0, 0, width, height)
+  // gl.viewport(0, 0, width, height)
 
   // Enables depth testing, which prevents triangles
   // from overlapping.
-  gl.enable(gl.DEPTH_TEST)
+  // gl.enable(gl.DEPTH_TEST)
 
   // Enables face culling, which prevents triangles
   // being visible from behind.
-  gl.enable(gl.CULL_FACE)
+  // gl.enable(gl.CULL_FACE)
 
   // Binds the geometry and sets up the shader's attribute
   // locations accordingly.
-  geometry.bind(shader)
 
   // Updates our model/view/projection matrices, sending them
   // to the GPU as uniform variables that we can use in
   // `shaders/bunny.vert` and `shaders/bunny.frag`.
-  shader.uniforms.uProjection = projection
-  shader.uniforms.uView = view
-  shader.uniforms.uModel = model
-
-  // Finally: draws the bunny to the screen! The rest is
-  // handled in our shaders.
-  geometry.draw(gl.TRIANGLES)
+  
+  // shader.uniforms.uProjection = projection
+  // shader.uniforms.uView = view
+  // shader.uniforms.uModel = model
+  
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	program.bind();
+	quad.bind(program);
+  
+	program.uniforms.uSunPos = [0, Math.cos(theta) * 0.3 + 0.2, -1];
+	
+	quad.draw();
 }
